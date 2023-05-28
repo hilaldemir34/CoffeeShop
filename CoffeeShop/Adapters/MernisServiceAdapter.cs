2601b4bd33b40ffd44c901a6434258d2172f6938
Hilal Demir <126691643+hilaldemir34@users.noreply.github.com>
2023-05-28 15:40:21 +0300

    Proje dosyası ekle.

---
+++ CoffeeShop/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeShop.Abstract;
+using CoffeeShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            MernisServiceReference.KPSPublicSoapClient client=new MernisServiceReference.KPSPublicSoapClient();
+            return client.TCKimlikNoDogrulaAsync(Convert.ToInt16( customer.NationalityId),customer.FirstName.ToUpper(),
+                customer.LastName.ToUpper(),customer.DateOfBirth.Year);
+        }
+    }
+}
---
+++ CoffeeShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeShop.Abstract;
+using CoffeeShop.Concrete;
+using CoffeeShop.Entities;
+using System;
+
+namespace CoffeeShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(2, 6, 1999), FirstName = "Jesica", LastName = "Kith", NationalityId = "18965742598" });
+        }
+    }
+}
